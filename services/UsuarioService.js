c4c3f0e3af67eb6ea45cdf2ed30151cdd37c917b
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-04-07 16:24:46 -0300

    feat: validaçoes da telas login e cadastro, integraçao API.

---
+++ services/UsuarioService.js
@@ -0,0 +1,26 @@
+import DevagramApiService from "./DevagramApiService";
+ 
+
+export default class UsuarioService extends DevagramApiService {
+    async login(credenciais) {
+        const { data } = await this.post('/login', credenciais);
+
+        localStorage.setItem("nome", data.nome);
+        localStorage.setItem("email", data.email);
+        localStorage.setItem("token", data.token);
+
+        const usuario = await this.get('/usuario');
+        localStorage.setItem('id', usuario.data._id);
+
+        if (usuario.data.avatar) {
+            localStorage.setItem("avatar", usuario.data.avatar);
+        }
+    }
+
+    
+    async cadastro(dados) {
+        return this.post('/cadastro', dados);
+    }
+
+    
+}
